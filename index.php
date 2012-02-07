756dde3c61ffb984046861593d76a35670de5628
Julian Kipka <julian.kipka@googlemail.com>
2012-02-07 20:26:40 +0100

    Added index.php and start design the upper area
    Signed-off-by: Julian Kipka <julian.kipka@googlemail.com>

---
+++ index.php
@@ -0,0 +1,53 @@
+<!-- for offline designing purpose -->
+<style type="text/css">
+	@import url("style.css");
+</style>
+<body>
+<div id="wrapper">
+
+    <div id="headerMenu">
+        <ul>
+          <li>Rss</li>
+          <li>About</li>
+        </ul>
+    </div>
+    
+    <div id="logoBox">
+    	<div id="main"><? bloginfo("name") ?>Bla</div>
+        <div id="sub"><? wp_title() ?>blub</div>
+    </div>
+    
+    <div id="navBar">
+      <div id="mainMenu">
+      	<ul>
+        	<li>Home</li>
+            <li>News</li>
+        </ul>
+      </div> 
+   	  <div id="search">
+        	<form>
+        	  <input type="text" name="search" id="searchfield">
+       	  </form>
+        </div>
+    </div>
+    
+    <div id="socialBar">
+    	<div id="slider">
+        	Placeholder
+        </div>
+        
+        <div id="socialBox">
+        	<div id="subscribe">
+            </div>
+            <div id="rss">
+            </div>
+            <div id="twitter">
+            </div>
+            <div id="facebook">
+            </div>
+        </div>
+    </div>
+    
+</div>
+</body>
+
