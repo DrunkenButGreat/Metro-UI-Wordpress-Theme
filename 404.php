f706a0574b15fc063d0ecab8a16eba2f80247a40
Julian Kipka <julian.kipka@googlemail.com>
2012-02-24 01:51:02 +0100

    Added 404.php and some Images
    Signed-off-by: Julian Kipka <julian.kipka@googlemail.com>

---
+++ 404.php
@@ -0,0 +1,11 @@
+<? get_header(); ?>
+	<div id="404">
+    	<div id="header">
+        	404 Requestet Site not found
+        </div>
+        <div id="text">
+        	Ooops.... it seems I made a mistake o.o I'am very sorry for that an try to avoid these ^^
+        </div>
+    </div>
+<?php get_footer(); ?>
+
